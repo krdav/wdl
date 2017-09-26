cf4601b2888eaeb84cd437286d0d91761f97dddc
krdav <krdav@cbs.dtu.dk>
2017-09-26 15:36:25 +0200

    WES

---
+++ scripts/WES_normal_tumor_somatic_SNV/hg38/ex_cromwell.sh
@@ -0,0 +1,3 @@
+
+java -Dconfig.file=/home/projects/cu_10098/apps/cromwell.conf -jar /home/projects/cu_10098/apps/src/cromwell/target/scala-2.12/cromwell-30-c4b562f-SNAP.jar run WES_normal_tumor_somatic_SNV.wdl -i WES_normal_tumor_somatic_SNV.inputs.json
+
