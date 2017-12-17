0cb632ead7c24ecf75e2a099610a0485a21ec694
krdav <krdav@cbs.dtu.dk>
2017-12-17 07:04:25 +0100

    ff

--- scripts/WGS_normal_RNAseq_tumor_SNV/hg37/ex_cromwell.sh
+++ scripts/WGS_normal_RNAseq_tumor_SNV/hg37/ex_cromwell.sh
@@ -10,3 +10,4 @@ module load star/2.5.3a
 # -Dcall-caching.enabled=false
 java -Dconfig.file=/home/projects/dp_00005/apps/cromwell.conf -jar /home/projects/dp_00005/apps/src/cromwell/target/scala-2.12/cromwell-30-c4b562f-SNAP.jar run WGS_normal_RNAseq_tumor_SNV.wdl -i WGS_normal_RNAseq_tumor_SNV.inputs.json
 
+
